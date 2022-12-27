f182053b2881da68e936bdc43a68ee4011b4e53f
Masaki Murooka <m-murooka@aist.go.jp>
2022-12-27 10:19:04 +0900

    Add Teleop state.

---
+++ include/LocomanipController/states/TeleopState.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <LocomanipController/State.h>
+
+#include <geometry_msgs/Twist.h>
+#include <ros/callback_queue.h>
+#include <ros/ros.h>
+
+namespace LMC
+{
+/** \brief FSM state to walk with teleoperation. */
+struct TeleopState : State
+{
+public:
+  /** \brief Start. */
+  void start(mc_control::fsm::Controller & ctl) override;
+
+  /** \brief Run. */
+  bool run(mc_control::fsm::Controller & ctl) override;
+
+  /** \brief Teardown. */
+  void teardown(mc_control::fsm::Controller & ctl) override;
+
+protected:
+  /** \brief ROS callback of twist topic. */
+  void twistCallback(const geometry_msgs::Twist::ConstPtr & twistMsg);
+
+protected:
+  //! Relative target velocity of foot midpose (x [m/s], y [m/s], theta [rad/s])
+  Eigen::Vector3d targetVel_ = Eigen::Vector3d::Zero();
+
+  //! Scale to convert twist message to target velocity (x, y, theta)
+  Eigen::Vector3d velScale_ = Eigen::Vector3d::Ones();
+
+  //! ROS variables
+  //! @{
+  std::unique_ptr<ros::NodeHandle> nh_;
+  ros::CallbackQueue callbackQueue_;
+  ros::Subscriber twistSub_;
+  //! @}
+};
+} // namespace LMC
---
+++ src/states/TeleopState.cpp
@@ -0,0 +1,103 @@
+#include <mc_rtc/gui/Button.h>
+#include <mc_rtc/ros.h>
+
+#include <LocomanipController/LocomanipController.h>
+#include <LocomanipController/ManipManager.h>
+#include <LocomanipController/states/TeleopState.h>
+
+using namespace LMC;
+
+void TeleopState::start(mc_control::fsm::Controller & _ctl)
+{
+  State::start(_ctl);
+
+  // Skip if ROS is not initialized
+  if(!mc_rtc::ROSBridge::get_node_handle())
+  {
+    mc_rtc::log::warning("[TeleopState] ROS is not initialized.");
+    output("OK");
+    return;
+  }
+
+  // Load configuration
+  std::string twistTopicName = "/cmd_vel";
+  if(config_.has("configs"))
+  {
+    if(config_("configs").has("velScale"))
+    {
+      velScale_ = config_("configs")("velScale");
+      velScale_[2] = mc_rtc::constants::toRad(velScale_[2]);
+    }
+    config_("configs")("twistTopicName", twistTopicName);
+  }
+
+  // Setup ROS
+  nh_ = std::make_unique<ros::NodeHandle>();
+  // Use a dedicated queue so as not to call callbacks of other modules
+  nh_->setCallbackQueue(&callbackQueue_);
+  twistSub_ = nh_->subscribe<geometry_msgs::Twist>(twistTopicName, 1, &TeleopState::twistCallback, this);
+
+  // Setup GUI
+  ctl().gui()->addElement({ctl().name(), "Teleop"},
+                          mc_rtc::gui::Button("StartTeleop", [this]() { ctl().manipManager_->startVelMode(); }));
+  ctl().gui()->addElement({ctl().name(), "Teleop", "State"},
+                          mc_rtc::gui::ArrayInput(
+                              "targetVel", {"x", "y", "theta"},
+                              [this]() -> Eigen::Vector3d {
+                                return Eigen::Vector3d(targetVel_[0], targetVel_[1],
+                                                       mc_rtc::constants::toDeg(targetVel_[2]));
+                              },
+                              [this](const Eigen::Vector3d & v) {
+                                targetVel_ = Eigen::Vector3d(v[0], v[1], mc_rtc::constants::toRad(v[2]));
+                              }));
+
+  output("OK");
+}
+
+bool TeleopState::run(mc_control::fsm::Controller &)
+{
+  // Finish if ROS is not initialized
+  if(!mc_rtc::ROSBridge::get_node_handle())
+  {
+    return true;
+  }
+
+  // Call ROS callback
+  callbackQueue_.callAvailable(ros::WallDuration());
+
+  // Update GUI
+  bool velMode = ctl().manipManager_->velMode();
+  if(velMode && ctl().gui()->hasElement({ctl().name(), "Teleop"}, "StartTeleop"))
+  {
+    ctl().gui()->addElement({ctl().name(), "Teleop"},
+                            mc_rtc::gui::Button("EndTeleop", [this]() { ctl().manipManager_->endVelMode(); }));
+    ctl().gui()->removeElement({ctl().name(), "Teleop"}, "StartTeleop");
+  }
+  else if(!velMode && ctl().gui()->hasElement({ctl().name(), "Teleop"}, "EndTeleop"))
+  {
+    ctl().gui()->addElement({ctl().name(), "Teleop"},
+                            mc_rtc::gui::Button("StartTeleop", [this]() { ctl().manipManager_->startVelMode(); }));
+    ctl().gui()->removeElement({ctl().name(), "Teleop"}, "EndTeleop");
+  }
+
+  // Set target velocity
+  if(ctl().manipManager_->velMode())
+  {
+    ctl().manipManager_->setRelativeVel(targetVel_);
+  }
+
+  return false;
+}
+
+void TeleopState::teardown(mc_control::fsm::Controller &)
+{
+  // Clean up GUI
+  ctl().gui()->removeCategory({ctl().name(), "Teleop"});
+}
+
+void TeleopState::twistCallback(const geometry_msgs::Twist::ConstPtr & twistMsg)
+{
+  targetVel_ = velScale_.cwiseProduct(Eigen::Vector3d(twistMsg->linear.x, twistMsg->linear.y, twistMsg->angular.z));
+}
+
+EXPORT_SINGLE_STATE("LMC::Teleop", TeleopState)
